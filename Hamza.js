75839bdb9ab3790f706a737f8221a0541c6b6a94
technical-mindset <73779880+technical-mindset@users.noreply.github.com>
2021-05-17 03:15:28 +0000

    Add files via upload

---
+++ Hamza.js
@@ -0,0 +1,23 @@
+//Pdf-1:-
+alert("Error! Please enter a valid password");
+alert("Welcome to JS Land...\nHappy Coding");
+alert("Welcome to JS Land");
+alert("Happy Coding\nPrevent this page from creating additional dialouges");
+alert("Hello... i can run JS through my web browser's console");
+//Pdf-2:-
+var username;
+var myName = 'Hamza';
+var message;
+message = "Hello World";
+alert(message);
+var stdName , stdAge , stdCourse;
+stdName = 'Hamza';
+stdAge = '21 years old';
+stdCourse = "Web and Mobile";
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+var email = "example@email.com";
+alert("My email is "+email);
+var x = docoumnet.getElementByTag().innerHTML = "<h1>Yah! I can cHTML content through JavaScript</h1>";
+//Pdf-3:-
+alert("");
